e9379f8e3d5d8b1e2a4c7e44cf693b44879b7e7d
Dev-Ekko <Ekko-NingYuan.Dong@users.noreply.github.com>
2020-01-31 16:25:02 +0800

    Vertical layout

---
+++ src/designer/RunMainWIndowVerticalLayout.py
@@ -0,0 +1,12 @@
+import sys
+import MainWindowVerticalLayout
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = MainWindowVerticalLayout.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
